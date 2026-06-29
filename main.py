9b8d08d3259247c5aab7090160ef9db4fde52cce
lulichon <lucasfacheradio@gmail.com>
2026-06-29 19:15:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 import yaml
 import requests
-import time
 import os
+from checker import check_ticketmaster
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = "2127560202"
-
-with open("concerts.yaml", "r") as f:
-    data = yaml.safe_load(f)
-
-concerts = data["concerts"]
 
 def send_telegram(message):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
@@ -21,12 +16,20 @@ def send_telegram(message):
 
     requests.post(url, data=payload)
 
+with open("concerts.yaml", "r") as f:
+    data = yaml.safe_load(f)
+
+concerts = data["concerts"]
+
 for concert in concerts:
     nom = concert["nom"]
     url = concert["url"]
 
-    message = f"🎟 Test Ticketmaster Alert\n\nConcert surveillé : {nom}\n{url}"
+    available = check_ticketmaster(url)
 
-    send_telegram(message)
-
-print("Message envoyé.")
+    if available:
+        message = f"🚨 Billets détectés !\n\n🎟 {nom}\n{url}"
+        send_telegram(message)
+        print(f"{nom} disponible")
+    else:
+        print(f"{nom} indisponible")
