54047645df608c64174894e2a616e796b0078281
lulichon <lucasfacheradio@gmail.com>
2026-06-29 19:06:07 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import yaml
+import requests
+import time
+
+BOT_TOKEN = "8590192785:AAEv_lni7mhtNOJFNZc4FJuFZEnSwm7mTZs"
+CHAT_ID = "2127560202"
+
+with open("concerts.yaml", "r") as f:
+    data = yaml.safe_load(f)
+
+concerts = data["concerts"]
+
+def send_telegram(message):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+
+    requests.post(url, data=payload)
+
+for concert in concerts:
+    nom = concert["nom"]
+    url = concert["url"]
+
+    message = f"🎟 Test Ticketmaster Alert\n\nConcert surveillé : {nom}\n{url}"
+
+    send_telegram(message)
+
+print("Message envoyé.")
